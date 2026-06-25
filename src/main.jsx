5fe298427d9dad411859679755b135cb39ca68ac
Godswillkhojee <ardiumwing@gmail.com>
2026-06-26 04:03:11 +0530

    added cocktails menu

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Hero from './components/Hero.jsx'
+import Cocktails from './components/Cocktails.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App/>
     <Hero/>
-    <div className='h-dvh bg-black'></div>
+    <Cocktails  />
   </StrictMode>,
 )
